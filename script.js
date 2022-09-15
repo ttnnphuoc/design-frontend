d212efa86ce2454a3fecd9c6558a14fe0f03f984
Phuoc Tran (SM) <phuoc.tran@mediastep.com>
2022-09-15 17:53:36 +0700

    update js hide/show btn buy

--- script.js
+++ script.js
@@ -5,4 +5,11 @@ btns.forEach(el => el.addEventListener('mouseover', event => {
   if (btn) {
     btn.style.display = "block";
   }
-}));
+}, false));
+
+btns.forEach(el => el.addEventListener('mouseleave', event => {
+  var btn = el.querySelector("button");
+  if (btn) {
+    btn.style.display = "none";
+  }
+}, false));
