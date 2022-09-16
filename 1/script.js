2a141fdc2279acda454be3318639e03351a04430
Phuoc Tran (SM) <phuoc.tran@mediastep.com>
2022-09-16 17:57:41 +0700

    update menu mobile

--- 1/script.js
+++ 1/script.js
@@ -13,3 +13,12 @@ btns.forEach(el => el.addEventListener('mouseleave', event => {
     btn.style.display = "none";
   }
 }, false));
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
